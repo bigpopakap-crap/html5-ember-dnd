3fe9c24889bf2fe2738d2039619e0747a0af7798
Kapil Easwar <bigpopakap@gmail.com>
2017-09-24 10:49:39 -0700

    Added drag handles to the example

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   items: [
-    { key: 'A', text: 'Item A' },
-    { key: 'b', text: 'Item B' },
-    { key: 'c', text: 'Item C' },
-    { key: 'd', text: 'Item D' },
-    { key: 'e', text: 'Item E' }
+    { key: 'A', text: 'Item A', subtext: 'blah blah blah blah' },
+    { key: 'b', text: 'Item B', subtext: 'shmooop bloop' },
+    { key: 'c', text: 'Item C', subtext: 'yaddah yaddah yaddah' },
+    { key: 'd', text: 'Item D', subtext: 'lorem ipsum whatever' },
+    { key: 'e', text: 'Item E', subtext: 'meeeeehhhhhhhhhhhh' },
+    { key: 'f', text: 'Item F', subtext: '......' },
+    { key: 'g', text: 'Item G', subtext: 'uhhhhh wutt?' },
+    { key: 'h', text: 'Item H', subtext: 'ok now this is just getting annoying' },
+    { key: 'i', text: 'Item I', subtext: 'NO U' },
+    { key: 'j', text: 'Item J', subtext: 'I AM J, K' },
+    { key: 'k', text: 'Item K!', subtext: 'i am kapil' }
   ]
 });
