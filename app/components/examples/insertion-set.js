e7c848096452d2752daf35697913e2bd76dcf945
Kapil Easwar <bigpopakap@gmail.com>
2017-10-02 17:32:28 -0700

    Cleaned up duplicate code for the two examples, and added ability to add new items to the list

--- app/components/examples/insertion-set.js
+++ app/components/examples/insertion-set.js
@@ -1,25 +1,9 @@
 import Ember from 'ember';
+import SetExample from './set';
 
-export default Ember.Component.extend({
-  enableSorting: true,
-  resetAfterDropOutside: true,
-  resetAfterDragCancel: true,
-  enableAnimation: true,
-  enableTouch: true,
-  enableKeyboard: true,
-  animationDuration: 'fast',
-
-  items: [
-    { key: 'A', text: 'Item A', subtext: 'blah blah blah blah' },
-    { key: 'b', text: 'Item B', subtext: 'shmooop bloop' },
-    { key: 'c', text: 'Item C', subtext: 'yaddah yaddah yaddah' },
-    { key: 'd', text: 'Item D', subtext: 'lorem ipsum whatever' },
-    { key: 'e', text: 'Item E', subtext: 'meeeeehhhhhhhhhhhh' },
-    { key: 'f', text: 'Item F', subtext: '......' },
-    { key: 'g', text: 'Item G', subtext: 'uhhhhh wutt?' },
-    { key: 'h', text: 'Item H', subtext: 'ok now this is just getting annoying' },
-    { key: 'i', text: 'Item I', subtext: 'NO U' },
-    { key: 'j', text: 'Item J', subtext: 'I AM J, K' },
-    { key: 'k', text: 'Item K!', subtext: 'i am kapil' }
-  ]
+export default SetExample.extend({
+  layoutName: 'components/examples/set',
+  title: 'Insertion Set',
+  componentName: 'drag-drop/insertion-set',
+  containerClass: 'insertion-list-container'
 });
---
+++ app/components/examples/set.js
@@ -0,0 +1,61 @@
+import Ember from 'ember';
+
+function nextChar(c) {
+  return String.fromCharCode(c.charCodeAt(0) + 1);
+}
+
+export default Ember.Component.extend({
+  title: 'Generic Set Example',
+  componentName: null,
+  containerClass: null,
+
+  enableSorting: true,
+  resetAfterDropOutside: true,
+  resetAfterDragCancel: true,
+  enableAnimation: true,
+  enableTouch: true,
+  enableKeyboard: true,
+  animationDuration: 'fast',
+
+  containerSelector: Ember.computed('containerClass', function() {
+    return `.${this.get('containerClass')}`;
+  }),
+
+  init(...args) {
+    this._super(...args);
+
+    this.set('items', [
+      { key: 'A', text: 'Item A', subtext: 'blah blah blah blah' },
+      { key: 'b', text: 'Item B', subtext: 'shmooop bloop' },
+      { key: 'c', text: 'Item C', subtext: 'yaddah yaddah yaddah' },
+      { key: 'd', text: 'Item D', subtext: 'lorem ipsum whatever' },
+      { key: 'e', text: 'Item E', subtext: 'meeeeehhhhhhhhhhhh' },
+      { key: 'f', text: 'Item F', subtext: '......' }
+    ]);
+
+    this.set('nextKey', 'g');
+  },
+
+  actions: {
+    createItemFirst() {
+      const newItem = this._createItem();
+      this.get('items').insertAt(0, newItem);
+    },
+
+    createItemLast() {
+      const newItem = this._createItem();
+      const len = this.get('items.length') || 0;
+      this.get('items').insertAt(len, newItem);
+    }
+  },
+
+  _createItem() {
+    const nextKey = this.get('nextKey');
+    this.set('nextKey', nextChar(nextKey));
+    return {
+      key: nextKey,
+      text: `Item ${nextKey}`,
+      subtext: this.get('newItemText')
+    };
+  }
+});
